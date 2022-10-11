186c5f47206909cdb893c3726c1172385225d972
gabatk <gabriela.trybuszek@gmail.com>
2022-10-11 22:56:54 +0200

    Project completed.

--- script.js
+++ script.js
@@ -12,22 +12,46 @@ const API_KEY = "&appid=12e9bfdafd4cfa5bd865175329afad8f";
 const API_UNITS = "&units=metric";
 
 const getWeather = () => {
-  const city = input.value || "Florida";
+  const city = input.value || "Tokyo";
   const URL = API_LINK + city + API_KEY + API_UNITS;
 
-  axios.get(URL).then((res) => {
-    console.log(res.data);
-    const temp = res.data.main.temp;
-    const hum = res.data.main.humidity;
-    const status = Object.assign({}, ...res.data.weather);
+  axios
+    .get(URL)
+    .then((res) => {
+      console.log(res.data);
+      const temp = res.data.main.temp;
+      const hum = res.data.main.humidity;
+      const status = Object.assign({}, ...res.data.weather);
 
-    cityName.textContent = res.data.name;
-    temperature.textContent = Math.floor(temp) + "°C";
-    humidity.textContent = hum + "%";
-    weather.textContent = status.main;
+      warning.textContent = "";
+      input.value = "";
 
-    // console.log(res.data.weather[0].id); - dokładnie to samo co const status
-  });
+      if (status.id >= 200 && status.id < 300) {
+        photo.setAttribute("src", "/img/thunderstorm.png");
+      } else if (status.id >= 300 && status.id < 400) {
+        photo.setAttribute("src", "/img/drizzle.png");
+      } else if (status.id >= 500 && status.id < 600) {
+        photo.setAttribute("src", "/img/rain.png");
+      } else if (status.id >= 600 && status.id < 700) {
+        photo.setAttribute("src", "/img/fog.png");
+      } else if (status.id >= 700 && status.id < 800) {
+        photo.setAttribute("src", "/img/fog.png");
+      } else if (status.id === 800) {
+        photo.setAttribute("src", "/img/sun.png");
+      } else if (status.id > 800 && status.id < 900) {
+        photo.setAttribute("src", "/img/cloud.png");
+      } else {
+        photo.setAttribute("src", "/img/unknown.png");
+      }
+
+      cityName.textContent = res.data.name;
+      temperature.textContent = Math.floor(temp) + "°C";
+      humidity.textContent = hum + "%";
+      weather.textContent = status.main;
+
+      // console.log(res.data.weather[0].id); - dokładnie to samo co const status
+    })
+    .catch(() => (warning.textContent = "Enter the correct city name!"));
 };
 
 const enterKeyCheck = (e) => {
