81880a9a8245c52794259dda5767932a5cabbb81
gabatk <gabriela.trybuszek@gmail.com>
2022-10-11 20:17:33 +0200

    Some new elements implemented in JavaScript.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
 
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q={city'
+const API_KEY = '&appid=12e9bfdafd4cfa5bd865175329afad8f'
+const API_UNITS = '&units=metric'
