1982ab00286ba4e307eca4d4729a8c5c02120b19
gabatk <gabriela.trybuszek@gmail.com>
2022-10-11 21:22:21 +0200

    Some new features added.

--- script.js
+++ script.js
@@ -8,21 +8,27 @@ const temperature = document.querySelector(".temperature");
 const humidity = document.querySelector(".humidity");
 
 const API_LINK = "https://api.openweathermap.org/data/2.5/weather?q=";
-const API_KEY = '&appid=12e9bfdafd4cfa5bd865175329afad8f';
+const API_KEY = "&appid=12e9bfdafd4cfa5bd865175329afad8f";
 const API_UNITS = "&units=metric";
 
 const getWeather = () => {
-  const city = input.value || "Boston";
+  const city = input.value || "Florida";
   const URL = API_LINK + city + API_KEY + API_UNITS;
 
   axios.get(URL).then((res) => {
     console.log(res.data);
-    const temp = res.data.main.temp
-    const hum = res.data.main.humidity
-    cityName.textContent = res.data.name
-    temperature.textContent = Math.floor(temp) + '°C'
-    humidity.textContent = hum + '%'
-  })
+    const temp = res.data.main.temp;
+    const hum = res.data.main.humidity;
+    const status = Object.assign({}, ...res.data.weather);
+
+    cityName.textContent = res.data.name;
+    temperature.textContent = Math.floor(temp) + "°C";
+    humidity.textContent = hum + "%";
+    weather.textContent = status.main;
+
+    // console.log(res.data.weather[0].id); - dokładnie to samo co const status
+  });
 };
 
 getWeather();
+button.addEventListener("click", getWeather);
